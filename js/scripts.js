2030fcb6958a356fdc0029879b6d98f4724353a9
pstaal <peter.staal@gmail.com>
2021-03-22 11:05:50 +0100

    Add pokemons to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-let pokemonList = [];
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Caterpie',
+    height: 3,
+    types: ['grass', 'ground']
+  },
+  {
+    name: 'Rattata',
+    height: 5,
+    types: ['tackle', 'super fang']
+  },
+];
